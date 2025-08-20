7c40cb3fcd82ae7b9f9eb2fafa88467332848936
sujan-pakhrin <sujannntmg@gmail.com>
2025-08-20 17:54:29 +0545

    add auth

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,27 +9,58 @@ import AdminOrder from './pages/admin/Order'
 import AdminFeatures from './pages/admin/Features'
 import ShopLayout from './components/shop/Layout'
 import NotFound from './pages/NotFound/Index'
+import ShopHome from './pages/shop/Home'
+import ShopList from './pages/shop/Listing'
+import ShopAccount from './pages/shop/Account'
+import ShopCheckOut from './pages/shop/Checkout'
+import Auth from "./components/utils/Auth"
+import Unuthorized from './pages/NotFound/Unuthorized'
 
 
 function App() {
+  const isAuthenticated=true
+  const user={
+    name:"ram",
+    role:"admin"
+  };
+
 
   return (
     <>
       <div className="flex flex-col overflow-hidden bg-white">
         <Routes>
-          <Route path="/auth" element={<AuthLayout />}>
+          <Route path="/auth" element={
+            <Auth isAuthenticated={isAuthenticated} user={user}>
+              <AuthLayout />
+             </Auth>
+          }
+          >
             <Route path="login" element={<Login />} />
             <Route path="register" element={<Register />} />
           </Route>
-          <Route path="/admin" element={<AdminLayout />}>
+          <Route path="/admin" element={
+            <Auth isAuthenticated={isAuthenticated} user={user}>
+              <AdminLayout />
+             </Auth>
+          }>
             <Route path="dashboard" element={<AdminDashoboard />} />
             <Route path="product" element={<AdminProduct />} />
             <Route path="order" element={<AdminOrder />} />
             <Route path="features" element={<AdminFeatures />} />
           </Route>
-          <Route path="/shop" element={<ShopLayout />}>
+          <Route path="/shop" element={
+            <Auth isAuthenticated={isAuthenticated} user={user}>
+              <ShopLayout />
+            </Auth>
+          }>
+            <Route path="home" element={<ShopHome />} />
+            <Route path="list" element={<ShopList />} />
+            <Route path="account" element={<ShopAccount />} />
+            <Route path="checkout" element={<ShopCheckOut />} />
+
           </Route>
-          <Route path="*" element={<NotFound/>}/>
+          <Route path="unauth-page" element={<Unuthorized />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </div>
     </>
---
+++ frontend/src/pages/NotFound/Unuthorized.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import unuthorized from '../../assets/NotFound/unauth.svg'
+const Index = () => {
+  return (
+    <div className='h-[100vh] flex justify-center items-center'>
+        <img src={unuthorized} alt="" className='h-[80%]'/>
+    </div>
+  )
+}
+
+export default Index
